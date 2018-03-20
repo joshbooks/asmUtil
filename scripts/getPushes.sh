6b63775c570571bef3e8d5e27db30bb4e41ae5b1
Josh <joshbooks@gmail.com>
2018-03-20 01:04:48 -0700

    prune some unnecessary code

--- scripts/getPushes.sh
+++ scripts/getPushes.sh
@@ -9,8 +9,3 @@ execWithOpcode '  echo "$byte" >> opCodesToBePushed '
 
 tac opCodesToBePushed | paste -d ' ' - - | sed -e 's/\(..\) \(..\)/\tpush WORD 0x\1\2/' -e 's/^\(..\) $/\tpush WORD 0x\100/' >> pushAsm.asm
 
-cat pushAsm.asm | awk "/WORD 0x[8-9a-f].../"'{print;$1="";$2="";gsub("0x", "");;print}'
-
-
-
-
